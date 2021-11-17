ee8a2a739268abd1642cc71a4b19f3bb5a99ab1d
Guido <ppapaguido@gmail.com>
2021-11-17 21:08:06 +0100

    fine prima parte

--- js/script.js
+++ js/script.js
@@ -1,41 +1,53 @@
-const container = document.querySelector('.container');
-
-const difficoltaU = prompt('inserisci difficolta') ;
-
-
-switch (difficoltaU) {
-    default:
-        n = 100;
-        break;
-    case 'media':
-        n = 81;
-        break;
-    case 'difficile':
-        n = 49;
-        break;
-    case 'facile':
-        n = 100;
-        break;
-
-}
+let container = document.querySelector('.container');
 
 
 
-function addSquare() {
+// funzioni
+function createSquare(){
     const node = document.createElement('div');
     node.className = 'square';
 
     return node;
 }
 
-for (let i = 0; i < n; i++) {
-    const divEL = addSquare();
-    container.appendChild(divEL);
+function creazioneGriglia (valoreSwitchRiga, valoreColonna) {
+    // crea la riga
+    for (let i = 0 ; i < valoreSwitchRiga ; i++) {
+        for (let k = 0 ; k < valoreColonna  ; k++) {
+            const divEl = createSquare();
+            container.appendChild(divEl)
 
-    divEL.addEventListener('click', function() {
-        this.classList.add('clicked-true');
-   
-    
-    })
+            divEl.addEventListener('click', function(){
+                this.classList.add('clicked-true');
+            })
 
+            
+        }
+    }
 }
+
+
+// prendo imput da mettere come difficolta che poi andra nella funzione
+const diff = parseInt(prompt('inserisci difficolta : 1 facile , 2 medio, 3 difficile'));
+
+let numeroRighe;
+let numeroColonne;
+switch (diff) {
+    case 1: numeroRighe = 10;
+            numeroColonne = 10;
+    break;
+    case 2: numeroRighe = 9;
+            numeroColonne = 92;
+    break;
+    case 3: numeroRighe = 7;
+            numeroColonne = 7;
+    break;
+    default: numeroRighe = 10;
+             numeroColonne = 10;
+}
+
+let funzione = creazioneGriglia(numeroRighe,numeroColonne);
+
+
+// prendo il singolo div
+

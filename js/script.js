15a0271bf04afe466e6c66292a967b7aed0206d6
Guido <ppapaguido@gmail.com>
2021-11-18 14:45:29 +0100

    inserimento style inline tramite JS bottone funzionante

--- js/script.js
+++ js/script.js
@@ -1,53 +1,69 @@
 let container = document.querySelector('.container');
+let numeroRighe;
+let numeroColonne;
+const diff = 3 ;// parseInt(prompt('inserisci difficolta : 1 facile , 2 medio, 3 difficile'));
+const lvlSelector = parseInt(document.getElementById('livello').value);
+
+
+switch (lvlSelector) {
+    case 1: numeroRighe = 100;
+            numeroColonne = Math.sqrt(numeroRighe);
+
+    break;
+    case 2: numeroRighe = 81;
+           numeroColonne = Math.sqrt(numeroRighe);
+
+    break;
+    case 3: numeroRighe = 49;
+            numeroColonne = Math.sqrt(numeroRighe);
+
+    break;
+    default: numeroRighe = 10;
+             numeroColonne = Math.sqrt(numeroRighe);
+
+}
 
 
 
 // funzioni
 function createSquare(){
+    // mi creo il div
     const node = document.createElement('div');
+    // metto la classe 'square'
     node.className = 'square';
+    // creo costante con il valore da mettere nel html
+    const size = `calc(100% / ${numeroColonne})`;
+    // metto nell html del css inline con la width piu il valore del size
+    node.style.width = size;
+    // metto nell html del css inline con la height piu il valore del size
+    node.style.height = size;
 
     return node;
 }
 
-function creazioneGriglia (valoreSwitchRiga, valoreColonna) {
-    // crea la riga
-    for (let i = 0 ; i < valoreSwitchRiga ; i++) {
-        for (let k = 0 ; k < valoreColonna  ; k++) {
+function creazioneGriglia (valoreSwitchRiga) {
+
+
+    for (let i = 1 ; i <= valoreSwitchRiga ; i ++) {
+        {
             const divEl = createSquare();
             container.appendChild(divEl)
-
-            divEl.addEventListener('click', function(){
+            divEl.innerHTML = i;
+            
+            divEl.addEventListener('click', function (){
                 this.classList.add('clicked-true');
             })
-
-            
         }
-    }
+
+    };
 }
 
 
-// prendo imput da mettere come difficolta che poi andra nella funzione
-const diff = parseInt(prompt('inserisci difficolta : 1 facile , 2 medio, 3 difficile'));
+console.log('valore di lvlSelector',lvlSelector);
 
-let numeroRighe;
-let numeroColonne;
-switch (diff) {
-    case 1: numeroRighe = 10;
-            numeroColonne = 10;
-    break;
-    case 2: numeroRighe = 9;
-            numeroColonne = 92;
-    break;
-    case 3: numeroRighe = 7;
-            numeroColonne = 7;
-    break;
-    default: numeroRighe = 10;
-             numeroColonne = 10;
-}
+const buttonLvl = document.getElementById('play');
+buttonLvl.addEventListener('click',function (){
+    creazioneGriglia(numeroRighe)
+});
 
-let funzione = creazioneGriglia(numeroRighe,numeroColonne);
-
-
-// prendo il singolo div
-
+console.log('numero di colonne',numeroColonne);
